f2fb2611faccd7bb33f0950810e6fe2c0f94ac15
Amit G <50439011+asg240@users.noreply.github.com>
2019-10-23 16:32:11 -0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'; 
-import projects from './projects.js'; 
-import articles from './articles.js'; 
-import about from './about.js'; 
+import projects from './components/projects.js'; 
+import articles from './components/articles.js'; 
+import about from './components/about.js'; 
 import './App.css';
 
 function App() {
@@ -11,15 +11,15 @@ function App() {
     <div className="App">
 
       <Route exact path="/" component={projects}/>
-      <Route path="/articles" component={articles}/>
-      <Route path="/about" component={about}/>
+      <Route path="/components/articles" component={articles}/>
+      <Route path="/components/about" component={about}/>
 
       <div className='nav'>
         <div className='nav-sub'>
 
           <Link to="/" className="item">Projects</Link>
-          <Link to="/articles" className="item">Articles</Link>
-          <Link to="/about" className="item">About</Link>
+          <Link to="/components/articles" className="item">Articles</Link>
+          <Link to="/components/about" className="item">About</Link>
 
         </div>
       </div>
---
+++ src/components/about.js
@@ -0,0 +1,17 @@
+import React from "react"
+ 
+function about(props) {
+  return (
+    <div>
+      <h1>About</h1>
+   
+   <p>Born and raised in the San Francisco Bay Area, I have made a career in the automobile industry reaching into technician debugging and mechanical repair or said vehicles and systems. </p>
+   <p>I have a huge interest in anything technological, from AI to bare bones motor systems. With these interests, I now include system coding and development.</p>
+   <p>Navigate my work and feel free to contact me with any inquries. Ill respond accordingly. </p>
+   
+ </div>
+    </div>
+  )
+}
+ 
+export default about;
