aa12dedfc0ad63aabac51b2727af315989182aef
Amit G <50439011+asg240@users.noreply.github.com>
2019-11-03 22:50:29 -0800

    edited about

--- src/components/about.js
+++ src/components/about.js
@@ -7,9 +7,10 @@ function about(props) {
       <h1>Amit Gill</h1>
       <img src={me} width="30%" height="30%"/>
       <div className="about-text"> 
-      <p>Born and raised in the San Francisco Bay Area, I have made a career in the automobile industry reaching into technician debugging and mechanical repair or said vehicles and systems. </p>
-      <p>I have a huge interest in anything technological, from AI to bare bones motor systems. With these interests, I now include system coding and development.</p>
-      <p>Navigate my work and feel free to contact me with any inquries. Ill respond accordingly. </p>
+      <p>Born and raised in the San Francisco, Bay Area, I have largely made a career in the electric and conventional automobile industry.</p>
+      <p>From debugging modules, electrical systems, and codebase, to mechanical repair of said vehicles and systems. </p>
+      <p>I have a huge interest in anything involving technology, from AI to bare bones motor systems. With these interests, I now include system coding and development.</p>
+      <p>Navigate my work and feel free to contact me with any inquries. </p>
       </div>
     </div>
   )
